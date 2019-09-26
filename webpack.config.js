117e2f8a8059dbe2be75b26942d364062bceadb2
Francisco Dias <francisco.ffbhd@gmail.com>
2019-09-26 21:34:23 +0100

    Francisco Profile Slide

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: 'src/index.html',
       filename: 'index.html',
       inject: 'body'
     })
